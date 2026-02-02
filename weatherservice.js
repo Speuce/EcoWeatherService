6d6ff4ae2f899e8150c35dc45de4f6ab554c5442
chizaraanyanwu <144716570+chizaraanyanwu@users.noreply.github.com>
2026-02-02 10:29:39 -0600

    Modify weatherservice.js

--- weatherservice.js
+++ weatherservice.js
@@ -18,12 +18,7 @@ class WeatherService {
         return "Hot";
       } else if (temp > 20) {
         return "Warm";
-      } else if (temp > 10) {
-        return "Mild";
-      } else if (temp > 0) {
-        return "Cool";
-      } else if (temp > -10) {
-        return "Chilly";
+     
       } else if (temp > -20) {
         return "Bracing";
       } else {
